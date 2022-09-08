1aee799b36dfd83cbdc64b390df9609c4224efb5
MathiasVansteensel <83126193+MathiasVansteensel@users.noreply.github.com>
2022-09-08 19:53:08 +0200

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MathiasVansteensel.GIP._2022_2023.CrossPlatform.Networking;
-
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
