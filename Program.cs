3ea473ac67538dcc101e08bf6ebe410b00c49018
mathi <mathi@GINGY-PC>
2022-09-08 19:40:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MathiasVansteensel.GIP._2022_2023.CrossPlatform.Networking;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
